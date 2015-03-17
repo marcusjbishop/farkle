ab97d103f569615c091c055dcd482624e4896959
Marcus Bishop <marcus.bishop@gmail.com>
2015-03-17 11:26:22 -0500

    forget python

--- player.py
+++ player.py
@@ -1,12 +1,16 @@
-from score import *
-def decide(l,p): # l=list dice, p=points so far
+from possible import *
+pp=[possiblePoints(i) for i in range(1,7)]
+
+def decide(dice,p): # p=points so far
+  if type(dice)<>list or 6<len(l):
+    raise Exception("dice not a list of proper length")
   if type(p)<>int or p<0:
     raise Exception("p not a natural number")
-  s=points(l)
-  m=[]
-  while 1 in l:
-    l.remove(1)
-    l.append(1)
-  while 5 in l:
-    l.remove(5)
-    l.append(5)
+  m=[dice.count(i) for i in range(1,7)]
+  score=points(m)
+  keep=[x for x in dice if x==1 or x==5]
+  reroll=[x for x in dice if x<>1 or x<>5]
+  for x in subsets(reroll):
+    s=sum(y['prob']*(y['score']+score) for y in pp[len(x)])
+    print s
+  
