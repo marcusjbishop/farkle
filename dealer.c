291dfb84c8dc55e34857793e128c655c0c2a51d3
Marcus Bishop <marcus.bishop@gmail.com>
2015-03-22 12:20:59 -0500

    seems to vork, modulo the problem with teh player

--- dealer.c
+++ dealer.c
@@ -1,6 +1,9 @@
-#include<stdio.h> #include<unistd.h> #include<time.h>
+#include<stdio.h>
+#include<unistd.h>
+#include<time.h>
 #include<stdlib.h>
 #include<sys/select.h>
+#include<string.h>
 #include"score.c"
 
 void
@@ -12,9 +15,12 @@ roll(int *dice)
 }
 
 void
-printDice(int *v, char *s)
+printDice(int *v, char *s, int n)
 {
-  sprintf(s, "%d,%d,%d,%d,%d,%d", v[0], v[1], v[2], v[3], v[4], v[5]);
+  int		  i;
+  for (i = 0; i < n; i++)
+    s[i] = v[i] + 48;
+  s[n] = '\n';
 }
 
 int
@@ -24,9 +30,10 @@ main(void)
   int		  g        [2];
   int		  pid;
   fd_set	  wset  , rset;
-  char		  s       [12];
+  char		  s       [8];
   int		  dice     [6];
   int		  n;
+  struct timeval  t;
 
   if ((pipe(f)) < 0)
     perror("pipe failed");
@@ -39,39 +46,44 @@ main(void)
 
       close(f[0]);
     close(g[1]);
-
     FD_ZERO(&wset);
     FD_SET(f[1], &wset);
+    t.tv_sec = 2;
+    t.tv_usec = 0;
     srand(time(NULL));
     roll(dice);
-    printDice(dice, s);
-    n = select(f[1] + 1, NULL, &wset, NULL, NULL);
+    printDice(dice, s, 6);
+    printf("%s", s);
+    n = select(f[1] + 1, NULL, &wset, NULL, &t);
     printf("parent write select returned %d\n", n);
-    n = write(f[1], s, sizeof(s));
+    n = write(f[1], s, strlen(s));
     printf("parent wrote %d bytes of %s\n", n, s);
 
     FD_ZERO(&rset);
     FD_SET(g[0], &rset);
-    n = select(g[0] + 1, &rset, NULL, NULL, NULL);
+    n = select(g[0] + 1, &rset, NULL, NULL, &t);
+    if (n < 0) {
+      perror("Bad read pipe");
+      exit(1);
+    } else if (n == 0) {
+      fprintf(stdout, "Failed to read");
+      exit(1);
+    }
     printf("parent read select returned %d\n", n);
     n = read(g[0], s, 12);
     printf("parent read %d bytes of %s\n", n, s);
 
   } else {
     //Child
-    close(f[1]);
+      close(f[1]);
     close(g[0]);
     dup2(f[0], 0);
     dup2(g[1], 1);
     close(f[0]);
     close(g[1]);
+    setvbuf(stdout, NULL, _IOLBF, 6);
+    /* fgets(s,7,stdin); s[0]=100; puts(s); */
 
-    //read(0,s,13);
-    fgets(s,13,stdin);
-    s[0]=100;
-    //write(1,s,sizeof(s));
-    fprintf(stdout,s);
-
- //   execve("/Users/marcus/farkle/player.py", NULL, NULL);
+    execve("/Users/marcus/farkle/player.py", NULL, NULL);
   }
 }
